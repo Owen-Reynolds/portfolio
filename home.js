f0c79e472cdb655e3d9eaebcf6e5252f754bf9b9
Owen-Reynolds <oreynold@ashland.edu>
2024-11-07 20:06:30 -0500

    Changes

---
+++ home.js
@@ -0,0 +1,17 @@
+function scrollHome(){
+    document.getElementById('home').scrollIntoView({
+        behavior: "smooth"
+    })
+}
+
+function scrollProjects(){
+    document.getElementById('projects').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollAbout(){
+    document.getElementById('about').scrollIntoView({
+        behavior: "smooth"
+    })
+}
